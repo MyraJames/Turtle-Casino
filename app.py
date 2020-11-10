2ae0b916962843220845f9a2326a36a29d124dd5
Myra James <Treesmellgood@gmail.com>
2020-11-10 17:46:46 -0500

    New Update

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from turtle import *
 import time
 setup(width = 500, height = 200, startx =-200, starty = -600)
 
-bank = 1500
+wallet = 1500
+
+
 
 shape1 = Turtle()
 shape2 = Turtle()
@@ -25,11 +27,11 @@ shapelist = ["square", "circle", "triangle"]
 keep_playing = "true"
 
 while keep_playing == "true":
-  print("You have", bank, "points to wager.")
+  print("You have", wallet, "points to wager.")
   wager = int(input("What is your wager?"))
-  if wager > bank:
+  if wager > wallet:
       continue
-  bank = bank - wager
+  wallet = wallet - wager
   for i in range(3):
       shape1.shape(shapelist[i])
       time.sleep(0.1)
@@ -44,3 +46,35 @@ result2 = random.choice(shapelist)
 shape2.shape(result2)
 result3 = random.choice(shapelist)
 shape3.shape(result3)
+
+if result1 == result2 and result2 == result3:
+    if result1 == "square":
+        print ("YOU ARE GREAT! WINNER 3 SQUARES!")
+        wager = wager * 5
+        wallet = wallet + wager
+    elif result1 == "circle":
+        print ("YOU ARE GREAT! WINNER 3 CIRCLES!")
+        wager = wager * 15
+        wallet = wallet + wager
+    elif result == "triangle":
+        print ("YOU ARE GREAT! WINNER 3 TRIANGLES")
+        wager = wager * 20
+        wallet = wallet + wager
+    else:
+        print ("LOSER SPEND MORE MONEY AND TRY AGAIN :D")
+
+
+
+
+
+
+
+
+# welcome(wallet, pot)
+#   choice = user_choice()
+#   flip_result = coin_flip()
+#   wallet = payment(wallet, -5)
+#   overall_result = test_result(choice, flip_result)
+#   wallet = wallet_resolution(wallet, overall_result, pot)
+#   result_text(overall_result)
+#   play_again(wallet, original_amount)
